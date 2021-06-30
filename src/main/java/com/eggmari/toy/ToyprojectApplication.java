3fecfb07e9ff9f50ae8d7deab451e91d45946c4e
DESKTOP-GT9CG5T\subin <55Ihorai>
2021-06-30 21:02:47 +0900

    hello world!

--- src/main/java/com/eggmari/toy/ToyprojectApplication.java
+++ src/main/java/com/eggmari/toy/ToyprojectApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ToyprojectApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ToyprojectApplication.class, args);
 	}
 
