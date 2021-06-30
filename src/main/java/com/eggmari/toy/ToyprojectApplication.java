ec8bf5264d0a3fc296202f2dc6c2194e0ad899db
DESKTOP-GT9CG5T\subin <55Ihorai>
2021-06-30 20:56:06 +0900

    Initial commit

---
+++ src/main/java/com/eggmari/toy/ToyprojectApplication.java
@@ -0,0 +1,13 @@
+package com.eggmari.toy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eggmari/toy/ToyprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eggmari.toy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
