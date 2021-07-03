aab27805b68f6738aeb766126479f213fc4aa175
DESKTOP-GT9CG5T\subin <subin_95o1@naver.com>
2021-07-03 22:28:39 +0900

    깃 풀 심기

--- src/main/java/com/eggmari/toy/controller/MainController.java
+++ src/main/java/com/eggmari/toy/controller/MainController.java
@@ -4,11 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
-    @GetMapping(value = "/")
-    public String main(){
-        return "Main";
+
+    @RequestMapping(value = "/main")
+    public ModelAndView main(){
+        ModelAndView model = new ModelAndView();
+        System.out.println("sadsdas");
+
+        return model;
+       // return "Web-INF/jsp/Main.jsp";
     }
 }
--- src/main/webapp/WEB-INF/jsp/Main.jsp
+++ src/main/webapp/WEB-INF/jsp/Main.jsp
@@ -7,6 +7,6 @@
     <title>View Test Page</title>
 </head>
 <body>
-<h2>Hello, World!</h2>
+<h2>Hello, Worssld!</h2>
 </body>
 </html>
