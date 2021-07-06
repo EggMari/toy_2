d57525d4613c820cf93b33bbfc4196a49b11190c
DESKTOP-GT9CG5T\subin <subin_95o1@naver.com>
2021-07-06 20:27:11 +0900

    DB연결성공(프로퍼티 제외)

--- src/main/java/com/eggmari/toy/dto/OilPriceDto.java
+++ src/main/java/com/eggmari/toy/dto/OilPriceDto.java
@@ -1,15 +1,27 @@
 package com.eggmari.toy.dto;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Setter
+@Entity
 public class OilPriceDto {
-    private String oil_idx;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int oil_idx;
+    @Column(length = 20, nullable = false)
     private String oil_kind;
+    @Column(length = 20, nullable = false)
     private double oil_price;
+    @Column(length = 20, nullable = false)
     private double oil_price_Increase;
+    @Column(length = 20, nullable = false)
     private String sale_area;
 
 
