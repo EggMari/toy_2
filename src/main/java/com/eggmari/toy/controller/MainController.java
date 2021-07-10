526541a05f807b62c945d9c39825178f82bffd81
교자만두 <subin_95o1@naver.com>
2021-07-10 19:14:03 +0900

    암호화 유틸 추가

--- src/main/java/com/eggmari/toy/controller/MainController.java
+++ src/main/java/com/eggmari/toy/controller/MainController.java
@@ -1,10 +1,12 @@
 package com.eggmari.toy.controller;
 
+import com.eggmari.toy.util.Crypto;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,9 +15,10 @@ public class MainController {
     
     
     @RequestMapping(value = "/main")
-    public ModelAndView main(HttpServletRequest req){
+    public ModelAndView main(HttpServletRequest req) throws Exception {
         ModelAndView mav = new ModelAndView("template/template");
         req.setAttribute("CONTENT", "Main");
+
         return mav;
     }
 
