2ce629fdf165d2400435976e05e5d93f262fd3e4
DESKTOP-GT9CG5T\subin <subin_95o1@naver.com>
2021-07-13 22:20:59 +0900

    일배치 sysout 추가

--- src/main/java/com/eggmari/toy/service/AutoOilPriceGetter.java
+++ src/main/java/com/eggmari/toy/service/AutoOilPriceGetter.java
@@ -7,6 +7,8 @@ import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 
+import java.util.Date;
+
 @RequiredArgsConstructor
 public class AutoOilPriceGetter {
     @Autowired
@@ -14,6 +16,8 @@ public class AutoOilPriceGetter {
 
     @Scheduled(cron = "0 0 5 * * *")
     public void AutoOilGetter() throws ParseException {
+        Date date = new Date();
+        System.out.println("일배치 돌아감"+date);System.out.println("일배치 돌아감");System.out.println("일배치 돌아감");System.out.println("일배치 돌아감");System.out.println("일배치 돌아감");
        JSONObject json = oilPriceService.getOilPrice();
        oilPriceService.savePrices(json);
     }
