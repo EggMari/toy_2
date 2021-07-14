7a4b890b6eda34b8a785b15a452f4bc16344b3c0
DESKTOP-GT9CG5T\subin <subin_95o1@naver.com>
2021-07-14 19:46:44 +0900

    일배치 돌아가게 설정 ( SpringBootApplication 어노테이션 추가)

--- src/main/java/com/eggmari/toy/service/AutoOilPriceGetter.java
+++ src/main/java/com/eggmari/toy/service/AutoOilPriceGetter.java
@@ -5,11 +5,15 @@ import lombok.RequiredArgsConstructor;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
 import java.util.Date;
 
 @RequiredArgsConstructor
+@EnableScheduling
+@SpringBootApplication
 public class AutoOilPriceGetter {
     @Autowired
     private  OilPriceService oilPriceService;
@@ -17,7 +21,7 @@ public class AutoOilPriceGetter {
     @Scheduled(cron = "0 0 5 * * *")
     public void AutoOilGetter() throws ParseException {
         Date date = new Date();
-        System.out.println("일배치 돌아감"+date);System.out.println("일배치 돌아감");System.out.println("일배치 돌아감");System.out.println("일배치 돌아감");System.out.println("일배치 돌아감");
+        System.out.println("일배치 돌아감"+date);
        JSONObject json = oilPriceService.getOilPrice();
        oilPriceService.savePrices(json);
     }
